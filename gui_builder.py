a9c4e52ab0d20f2ddcbe65431623f93096c48a20
Phil Reinhold <pcreinhold@gmail.com>
2014-09-16 16:01:23 -0400

    require "gb_" prefix, add y plots, add checkbox for bool input

--- gui_builder.py
+++ gui_builder.py
@@ -1,6 +1,6 @@
 from inspect import getsource
-from PyQt4.QtGui import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QFormLayout, QDoubleSpinBox, QLineEdit, \
-    QPushButton, QApplication
+from PyQt4.QtGui import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout,\
+    QFormLayout, QLineEdit, QPushButton, QApplication, QCheckBox
 import ast
 import itertools
 from pyqtgraph import PlotWidget
@@ -9,7 +9,6 @@ from pyqtgraph.dockarea import DockArea, Dock
 
 def prettify(s):
     return ' '.join(w.capitalize() for w in s.split('_'))
-
 
 class GUIBuilder(QMainWindow):
     def __init__(self, instance):
@@ -33,47 +32,70 @@ class GUIBuilder(QMainWindow):
         seen_plot_items = []
 
         for node in ast.walk(ast.parse(getsource(type(instance)))):
-            if isinstance(node, ast.Call):
-                if node.func.id.startswith('get_'):
+            if isinstance(node, ast.Call) and isinstance(node.func, ast.Name):
+                if node.func.id.startswith('gb_get_') or node.func.id.startswith('gb_set_'):
                     segs = node.func.id.split('_')
-                    caster = __builtins__[segs[1]]
-                    name = "_".join(segs[2:])
+                    caster = __builtins__[segs[2]]
+                    name = "_".join(segs[3:])
+
                     if name in seen_form_items:
                         continue
                     seen_form_items.append(name)
-                    editor = QLineEdit()
-                    if node.args:
-                        if isinstance(node.args[0], ast.Num):
-                            init = node.args[0].n
-                        else:
-                            init = node.args[0].s
-                        editor.setText(str(caster(init)))
-                    __builtins__[node.func.id] = lambda init, e=editor, c=caster: c(e.text())
+
+                    if caster is bool:
+                        editor = QCheckBox()
+                        if node.func.id.startswith('gb_get_') and node.args:
+                            editor.setChecked(node.args[0].id == 'True')
+                        get_fn = lambda e=editor: e.isChecked()
+                        set_fn = lambda v, e=editor: e.setChecked(v)
+                    else:
+                        editor = QLineEdit()
+                        if node.func.id.startswith('gb_get_') and node.args:
+                            if isinstance(node.args[0], ast.Num):
+                                init = node.args[0].n
+                            else:
+                                init = node.args[0].s
+                            editor.setText(str(caster(init)))
+                        get_fn = lambda e=editor, c=caster: c(e.text())
+                        set_fn = lambda val, e=editor: e.setText(str(val))
+
+                    base_name = "_".join(segs[2:])
+                    get_name = "gb_get_" + base_name
+                    set_name = "gb_set_" + base_name
+                    __builtins__[get_name] = lambda init=0, get_fn=get_fn: get_fn()
+                    __builtins__[set_name] = lambda val, set_fn=set_fn: set_fn(val)
+
                     self.form_layout.addRow(prettify(name), editor)
 
-                if node.func.id.startswith('plot_xy_'):
+                if node.func.id.startswith('gb_plot_'):
                     segs = node.func.id.split("_")
-                    plot_type = segs[1]
-                    plot_name = segs[2]
-                    data_item_name = "_".join(segs[3:])
+                    plot_type = segs[2]
+                    plot_name = segs[3]
+                    if len(segs) >= 5:
+                        data_item_name = "_".join(segs[4:])
+                    else:
+                        data_item_name = ""
 
                     if (plot_name, data_item_name) in seen_plot_items:
                         continue
                     seen_plot_items.append((plot_name, data_item_name))
 
                     if plot_name not in self.plot_widgets:
-                        pw = PlotWidget()
-                        self.plot_widgets[plot_name] = pw
-                        self.plot_color_generators[plot_name] = itertools.cycle('rgb')
-                        pw.addLegend()
+                        if plot_type in ['y', 'xy']:
+                            pw = PlotWidget()
+                            self.plot_widgets[plot_name] = pw
+                            self.plot_color_generators[plot_name] = itertools.cycle('rgb')
+                            pw.addLegend()
+                        else:
+                            raise ValueError("Unknown plot type in {}: {}".format(node.func.id, plot_type))
                         dock = Dock(name=prettify(plot_name), widget=pw)
                         self.plots_layout.addDock(dock, 'above')
 
                     self.add_plot_item(node.func.id, plot_name, data_item_name)
 
             if isinstance(node, ast.FunctionDef):
-                if node.name.endswith("_button"):
-                    name = "_".join(node.name.split("_")[:-1])
+                if node.name.endswith("_gb_button"):
+                    name = "_".join(node.name.split("_")[:-2])
                     button = QPushButton(prettify(name))
                     button.clicked.connect(getattr(instance, node.name))
                     self.buttons_layout.addWidget(button)
@@ -82,17 +104,28 @@ class GUIBuilder(QMainWindow):
     def add_plot_item(self, fn_name, plot_name, data_item_name):
         plot_widget = self.plot_widgets[plot_name]
         color_generator = self.plot_color_generators[plot_name]
-        def plot_fn(x, y):
-            if data_item_name in self.plot_data_items:
-                self.plot_data_items[data_item_name].setData(x, y)
-            else:
-                self.plot_data_items[data_item_name] = plot_widget.plot(
-                    x, y, name=prettify(data_item_name), pen=color_generator.next()
-                )
+        plot_type = fn_name.split("_")[2]
+        if plot_type == "xy":
+            def plot_fn(x, y):
+                if data_item_name in self.plot_data_items:
+                    self.plot_data_items[data_item_name].setData(x, y)
+                else:
+                    self.plot_data_items[data_item_name] = plot_widget.plot(
+                        x, y, name=prettify(data_item_name), pen=color_generator.next()
+                    )
+        elif plot_type == "y":
+            def plot_fn(y):
+                if data_item_name in self.plot_data_items:
+                    self.plot_data_items[data_item_name].setData(y)
+                else:
+                    self.plot_data_items[data_item_name] = plot_widget.plot(
+                        y, name=prettify(data_item_name), pen=color_generator.next()
+                    )
+        else:
+            raise ValueError("Unknown plot type in {}: {}".format(fn_name, plot_type))
+
 
         __builtins__[fn_name] = plot_fn
-
-
 
 
 def make_gui(cls):
--- gui_builder_example.py
+++ gui_builder_example.py
@@ -1,18 +1,32 @@
 from gui_builder import make_gui
+from PyQt4.QtCore import QTimer
 from numpy import linspace, sin, cos, tan
 
 @make_gui
 class Window(object):
-    def make_plot_button(self):
-        f = get_float_frequency(3)
-        start = get_float_start(0)
-        stop = get_float_stop(10)
-        steps = get_int_step(100)
+    def make_plot_gb_button(self):
+        f = gb_get_float_frequency(3)
+        start = gb_get_float_start(0)
+        stop = gb_get_float_stop(10)
+        steps = gb_get_int_step(100)
         x = linspace(start, stop, steps)
         y = sin(f * x)
-        plot_xy_sinusoid_sin(x, sin(f*x))
-        plot_xy_sinusoid_cos(x, cos(f*x))
-        plot_xy_trig_tan(x, tan(f*x))
+        gb_plot_xy_sinusoid_sin(x, sin(f*x))
+        gb_plot_xy_sinusoid_cos(x, cos(f*x))
+        gb_plot_xy_trig_tan(x, tan(f*x))
+
+    def recurring_plot_gb_button(self):
+        self.timer = QTimer()
+        self.recurring_plot()
+
+    def recurring_plot(self):
+        f = gb_get_float_frequency()
+        gb_set_float_frequency(f+1)
+        self.make_plot_gb_button()
+        if gb_get_bool_halt(False):
+            gb_set_bool_halt(False)
+        else:
+            self.timer.singleShot(1000, self.recurring_plot)
 
 if __name__ == '__main__':
     Window()
